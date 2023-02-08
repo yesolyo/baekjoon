3534389deaa54d31958fc854bb5d756d84516259
AHN YERIM <104904719+yesolyo@users.noreply.github.com>
2023-02-08 19:49:45 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,5 +1,5 @@
-function solution(numbers,s=[0,1,2,3,4,5,6,7,8,9]) {
-    return s.reduce((a,b)=>a+b)-numbers.reduce((a,b)=>a+b);
+function solution(numbers) {
+    return 45-numbers.reduce((a,b)=>a+b,0);
 }
 //1. 1부터 9까지 더한다.
 //2. numbers안에 숫자 더한다.
