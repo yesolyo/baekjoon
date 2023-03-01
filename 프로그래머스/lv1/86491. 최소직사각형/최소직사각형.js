65f682f6bb7881ab3142802c20760a51aa52605e
AHN YERIM <104904719+yesolyo@users.noreply.github.com>
2023-03-01 22:39:19 +0900

    [level 1] Title: 최소직사각형, Time: 6.86 ms, Memory: 40.9 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,13 +1,10 @@
 function solution(sizes) {
-    let max1=0, max2=0
+    let max=[0,0];
     sizes.forEach((a) => a.sort((x,y)=>y-x));
-    for(let i=0; i<sizes.length;i++){
-        if(sizes[i][0] >max1){
-            max1=sizes[i][0];
-        }
-        if(sizes[i][1] >max2){
-            max2=sizes[i][1];
-        }
+    sizes.forEach(([w,h])=>{
+            if(w>max[0]) max[0]=w;
+            if(h>max[1]) max[1]=h;
     }
-    return max1*max2;
+    )
+    return max[0]*max[1];
 }
