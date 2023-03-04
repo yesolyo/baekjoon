a60497d051668b708d0d6ad38afc70814ccbdfbc
AHN YERIM <104904719+yesolyo@users.noreply.github.com>
2023-03-04 21:22:58 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,4 +1,15 @@
-function solution(s) {
+/*function solution(s) {
     let words=[/zero/gi,/one/gi,/two/gi,/three/gi,/four/gi,/five/gi,/six/gi,/seven/gi,/eight/gi,/nine/gi]
     words.forEach((e,i)=> s = s.replace(e,i));
-    return Number(s);}
+    return Number(s);
+}*/
+
+function solution(s) {
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let answer = s;
+    for(let i=0; i<numbers.length;i++){
+        let arr = answer.split(numbers[i]);
+        answer=arr.join(i);
+    }
+    return Number(answer);
+}
