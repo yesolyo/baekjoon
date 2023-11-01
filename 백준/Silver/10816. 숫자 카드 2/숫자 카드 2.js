ea9a862daf20c218d9f94549eead1806cc6c03d5
AHN YERIM <104904719+yesolyo@users.noreply.github.com>
2023-11-01 12:45:22 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 796 ms, Memory: 128164 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -1,17 +1,12 @@
 let fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let result = fs.readFileSync(filePath).toString().split("\n");
-const nList = result[1].split(" ");
+const nLlist = result[1].split(" ");
 const mList = result[3].split(" ");
-const map = new Map();
-nList.forEach((n) => {
-  if (map.has(n)) map.set(n, map.get(n) + 1);
-  else map.set(n, 1);
-});
 
-mList.forEach((m, i) => {
-  if (map.has(m)) mList[i] = map.get(m);
-  else mList[i] = 0;
-});
+const map = new Map();
+
+nLlist.forEach((n) => map.set(n, map.get(n) + 1 || 1));
+mList.forEach((m, i) => (mList[i] = map.get(m) || 0));
 
 console.log(mList.join(" "));
