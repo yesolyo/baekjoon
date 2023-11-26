fb970e2d95df8f576770cda3948a4428856fd62d
AHN YERIM <104904719+yesolyo@users.noreply.github.com>
2023-11-26 16:35:50 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 480 ms, Memory: 54480 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,29 @@
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath, "utf-8").trim().split("\n");
+
+const changeXYList = (stringList) => {
+  const XYList = [];
+  for (let i = 1; i < stringList.length; i++) {
+    XYList.push(stringList[i].split(" ").map(Number));
+  }
+  return XYList;
+};
+
+const sortedNumber = (numberList) => {
+  numberList.sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1] - b[1];
+    }
+    return a[0] - b[0];
+  });
+  return numberList;
+};
+
+const XYList = changeXYList(input);
+const sortedNumberList = sortedNumber(XYList);
+let answer = "";
+for (let i = 0; i < sortedNumberList.length; i++) {
+  answer += sortedNumberList[i][0] + " " + sortedNumberList[i][1] + "\n";
+}
+console.log(answer);
