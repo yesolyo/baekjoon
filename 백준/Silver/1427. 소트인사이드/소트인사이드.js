6d3951f7ae4849525356ff99386526c06fa0f476
AHN YERIM <104904719+yesolyo@users.noreply.github.com>
2023-11-26 18:43:39 +0900

    [Silver V] Title: 소트인사이드, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.js
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -1,8 +1,23 @@
 let fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs.readFileSync(filePath, "utf-8").trim().split("").map(Number);
-
-const getSortedNumbers = (numberList) => {
-  return [...numberList].sort((a, b) => b - a);
+const input = fs.readFileSync(filePath, "utf-8").trim();
+const getFrequencyList = (input) => {
+  const frequencyList = Array(10).fill(0);
+  for (let x of input) {
+    frequencyList[Number(x)]++;
+  }
+  return frequencyList;
 };
-console.log(getSortedNumbers(input).join(""));
+
+const getSortedNumbers = (frequencyList) => {
+  let answer = "";
+  for (let i = frequencyList.length - 1; i >= 0; i--) {
+    for (let j = 0; j < frequencyList[i]; j++) {
+      answer += i + "";
+    }
+  }
+  return answer;
+};
+const frequencyList = getFrequencyList(input);
+const answer = getSortedNumbers(frequencyList);
+console.log(answer);
