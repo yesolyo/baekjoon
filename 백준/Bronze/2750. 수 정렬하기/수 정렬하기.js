f85231e14f93eed3da7bea576af1095792b91f9c
AHN YERIM <104904719+yesolyo@users.noreply.github.com>
2023-11-26 15:50:04 +0900

    [Bronze II] Title: 수 정렬하기, Time: 208 ms, Memory: 11264 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -0,0 +1,23 @@
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath, "utf-8").trim().split("\n").map(Number);
+
+const addNumberToList = (list) => {
+  const numberList = [];
+  for (let i = 1; i < list.length; i++) {
+    numberList.push(list[i]);
+  }
+  return numberList;
+};
+
+const sortedNumber = (numberList) => {
+  numberList.sort((a, b) => a - b);
+  return numberList;
+};
+
+const newNumberList = addNumberToList(input);
+const sortedNumberList = sortedNumber(newNumberList);
+
+sortedNumberList.forEach((number) => {
+  console.log(number);
+});
