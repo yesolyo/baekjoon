d402a6ec1587dc5217e4fcf1bec1b09d6cabaccf
AHN YERIM <104904719+yesolyo@users.noreply.github.com>
2023-11-27 20:59:16 +0900

    [Silver I] Title: 신입 사원, Time: 3984 ms, Memory: 392392 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.js
@@ -0,0 +1,30 @@
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath, "utf-8").trim().split("\n");
+
+let testCase = Number(input[0]);
+let line = 1;
+
+for (let tc = 0; tc < testCase; tc++) {
+  let n = Number(input[line]);
+  let arr = [];
+  for (let i = line + 1; i <= line + n; i++) {
+    let data = input[i].split(" ").map(Number);
+    arr.push(data);
+  }
+
+  arr.sort((x, y) => x[0] - y[0]);
+
+  let count = 0;
+  let minValue = 100001;
+
+  for (let [x, y] of arr) {
+    if (y < minValue) {
+      minValue = y;
+      count++;
+    }
+  }
+  console.log(count);
+
+  line += n + 1;
+}
