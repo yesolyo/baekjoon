241f9aac470fce9abe80672d85a65ae881bc57c9
AHN YERIM <104904719+yesolyo@users.noreply.github.com>
2023-11-28 09:57:01 +0900

    [Silver III] Title: 주유소, Time: 296 ms, Memory: 37220 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -2,22 +2,23 @@ let fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath, "utf-8").trim().split("\n");
 
+const n = Number(input[0]);
 const roadLength = input[1].split(" ").map(Number);
 const oilPrice = input[2].split(" ").map(Number);
 
-const getTotalPrice = (roadLength, oilPrice) => {
+const getTotalPrice = (n, roadLength, oilPrice) => {
   let minValue = oilPrice[0];
-  let totalPrice = oilPrice[0] * roadLength[0];
+  let totalPrice = BigInt(0);
 
-  for (let i = 1; i < oilPrice.length - 1; i++) {
-    if (minValue > oilPrice[i]) {
-      minValue = oilPrice[i];
-      totalPrice += minValue * roadLength[i];
-    } else {
-      totalPrice += minValue * roadLength[i];
-    }
+  for (let i = 1; i < n; i++) {
+    minValue = Math.min(minValue, oilPrice[i]);
+    oilPrice[i] = minValue;
   }
-  return totalPrice;
+
+  for (let i = 0; i < n - 1; i++) {
+    totalPrice += BigInt(oilPrice[i]) * BigInt(roadLength[i]);
+  }
+  return String(totalPrice);
 };
 
-console.log(getTotalPrice(roadLength, oilPrice));
+console.log(getTotalPrice(n, roadLength, oilPrice));
